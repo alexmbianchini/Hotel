f381d41339c1afbc75855ef80a74b1ce46e0ab6d
fabroinjection <84641558+fabroinjection@users.noreply.github.com>
2021-09-05 12:33:04 -0300

    BDHelper completada

--- Hotel/Datos/DBHelper.cs
+++ Hotel/Datos/DBHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,23 @@ namespace Hotel.Datos
 {
     class DBHelper
     {
+        private SqlConnection conexion = new SqlConnection();
+        private SqlCommand comando = new SqlCommand();
+
+        private string cadenaConexion = @"Data Source=sql5103.site4now.net;Initial Catalog = db_a78fa0_hotel; Persist Security Info=True;User ID = db_a78fa0_hotel_admin";
+
+        public DataTable consultar(string consultaSQL)
+        {
+            DataTable tabla = new DataTable();
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = consultaSQL;
+            tabla.Load(comando.ExecuteReader());
+            conexion.Close();
+            return tabla;
+        }
+    }
     }
 }
