e987426098baeac094c4bdb377b4b291576f9d57
Alex <alexmaxbianchini@hotmail.com>
2021-09-05 13:19:06 -0300

    CambiosDetalleSerCuenta

---
+++ Hotel/Negocio/DetalleServicioCuenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Negocio
+{
+    class DetalleServicioCuenta
+    {
+        public int NroCuenta { get; set; }
+        public int NroHabitacion { get; set; }
+        public int CodProducto { get; set; }
+        public float PrecioUnitario { get; set; }
+        public DateTime FechaHora { get; set; }
+        public bool BorradoLogico { get; set; }
+        public int IdDetalle { get; set; }
+    }
+}
