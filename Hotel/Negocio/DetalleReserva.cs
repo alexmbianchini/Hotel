05c7bc062ae47c6af9307937658d42b2a7527b9d
fabroinjection <84641558+fabroinjection@users.noreply.github.com>
2021-09-05 13:18:26 -0300

    Clase DetalleReserva

---
+++ Hotel/Negocio/DetalleReserva.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Negocio
+{
+    class DetalleReserva
+    {
+        public int IdDetalle { get; set; }
+        public int IdReserva { get; set; }
+        public int NroHabitacion { get; set; }
+        public float PrecioUnitarioHabitacion { get; set; }
+        public bool BorradoLogico { get; set; }
+    }
+}
