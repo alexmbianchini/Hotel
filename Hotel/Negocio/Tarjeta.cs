32b5e963638595a4cb80b4afc9159613f800da27
Lucia Reale <lucireale@gmail.com>
2021-09-05 13:10:44 -0300

    Tarjeta2

---
+++ Hotel/Negocio/Tarjeta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Negocio
+{
+    class Tarjeta
+    {
+        public int IdTarjeta { get; set; }
+
+        public int NroTarjeta { get; set; }
+
+        public int CodTipoTarjeta { get; set; }
+
+        public DateTime FechaCaducidad { get; set; }
+
+        public string NroPasaporte { get; set; }
+    }
+}
