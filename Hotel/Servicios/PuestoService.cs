caa48d3d0f751a024b06a8d25aa0129e93dc22a4
fabroinjection <84641558+fabroinjection@users.noreply.github.com>
2021-09-16 16:27:21 -0300

    Más refinamiento en capas de EstadoHabitacion y Puesto

---
+++ Hotel/Servicios/PuestoService.cs
@@ -0,0 +1,26 @@
+﻿using Hotel.Datos.Dao;
+using Hotel.Datos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Servicios
+{
+    class PuestoService
+    {
+        private IPuesto dao;
+        public PuestoService()
+        {
+            dao = new PuestoDao();
+        }
+
+        public DataTable RecuperarTodos()
+        {
+            return dao.RecuperarTodos();
+        }
+
+    }
+}
